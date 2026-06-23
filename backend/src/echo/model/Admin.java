76010e716c51ce16a034ea1c585ef44f74ce89e4
MR-Mahdavi-N <mohammadrezamahdavinejad@gmail.com>
2026-06-23 23:29:24 +0330

    [Add] model/ User and Admin models with auth-related fields

--- backend/src/echo/model/Admin.java
+++ backend/src/echo/model/Admin.java
@@ -1,4 +1,27 @@
 package echo.model;
 
 public class Admin {
+    private String username;
+    private String passwordHash;
+
+    // constructor
+    public Admin(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    // getters and setters
+    public String getUsername() {
+        return username;
+    }
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 }
--- backend/src/echo/model/User.java
+++ backend/src/echo/model/User.java
@@ -1,4 +1,101 @@
 package echo.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String username;
+    private final String id;
+    private String passwordHash;
+    private String profileImagePath;
+    private boolean locked;
+    private LocalDateTime lockUntil;
+    private final List<String> contacts;
+    private final List<String> blockedUsers;
+    
+    // constructor
+    public User(String username, String id, String passwordHash) {
+        this.username = username;
+        this.id = id;
+        this.passwordHash = passwordHash;
+        this.profileImagePath = null;
+        this.locked = false;
+        this.lockUntil = null;
+        this.contacts = new ArrayList<>();
+        this.blockedUsers = new ArrayList<>();
+    }    
+
+    // getters and setters
+    public String getUsername() {
+        return this.username;
+    }
+    public String getId() {
+        return this.id;
+    }
+    public String getPasswordHash() {
+        return this.passwordHash;
+    }
+    public String getProfileImagePath() {
+        return this.profileImagePath;
+    }
+    public boolean getLocked() {
+        return this.locked;
+    }
+    public LocalDateTime getLockUntil() {
+        return this.lockUntil;
+    }
+    public List<String> getContacts() {
+        return this.contacts;
+    }
+    public List<String> getBlockedUsers() {
+        return this.blockedUsers;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+    public void setProfileImagePath(String profileImagePath) {
+        this.profileImagePath = profileImagePath;
+    }
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+    public void setLockUntil(LocalDateTime lockUntil) {
+        this.lockUntil = lockUntil;
+    }
+    
+    // eauals, hashCode and toString
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!this.id.equals(other.id))
+            return false;
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "User [username = " + this.username + ", id = " + this.id + ", passwordHash = " + this.passwordHash + ", profileImagePath = "
+                + profileImagePath + ", contacts = " + this.contacts + ", blockedUsers = " + this.blockedUsers + ", locked = " + this.locked
+                + ", lockUntil = " + this.lockUntil + "]";
+    }
 }
