14dd9c87e7e292dc03a7f04148b10f553e1fe83c
MR-Mahdavi-N <mohammadrezamahdavinejad@gmail.com>
2026-06-23 23:39:07 +0330

    [Add] util/ IdGenerator and DateTimeUtil helper classes

--- backend/src/echo/util/DateTimeUtil.java
+++ backend/src/echo/util/DateTimeUtil.java
@@ -1,4 +1,42 @@
 package echo.util;
 
+import java.time.LocalDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DateTimeUtil {
+    // static method for convert String to LocalDateTime
+    private static final Pattern pattern = Pattern.compile("Date: (\\d{4})-(\\d{2})-(\\d{2}), Time: (\\d{2}):(\\d{2}):(\\d{2})");
+    public static LocalDateTime parse(String dateTime) {
+        Matcher matcher = pattern.matcher(dateTime);
+        int year = Integer.parseInt(matcher.group(1));
+        int month = Integer.parseInt(matcher.group(2));
+        int day = Integer.parseInt(matcher.group(3));
+        int hour = Integer.parseInt(matcher.group(4));
+        int minute = Integer.parseInt(matcher.group(5));
+        int second = Integer.parseInt(matcher.group(6));
+        return LocalDateTime.of(year, month, day, hour, minute, second);
+    }
+
+    // static method for convert LocalDateTime to String
+    public static String format(LocalDateTime time) {
+        return String.format("Date: %04d-%02d-%02d, Time: %02d:%02d:%02d",
+                                    time.getYear(), time.getMonthValue(), time.getDayOfMonth(),
+                                    time.getHour(), time.getMinute(), time.getSecond());
+    }
+
+    // static method to get current date and time
+    public static LocalDateTime now() {
+        return LocalDateTime.now();
+    }
+
+    // static method for checking user account lock
+    public static boolean isFinished(LocalDateTime time) {
+        return LocalDateTime.now().isAfter(time);
+    } 
+
+    // static method for lock user account
+    public static LocalDateTime plusMinutes(LocalDateTime currentTime, long minutes) {
+        return currentTime.plusMinutes(minutes);
+    }
 }
--- backend/src/echo/util/IdGenerator.java
+++ backend/src/echo/util/IdGenerator.java
@@ -1,4 +1,34 @@
 package echo.util;
 
 public class IdGenerator {
+    private static long userCounter = 1;
+    private static long messageCounter = 1;
+    private static long groupCounter = 1;
+    private static long conversationCounter = 1;
+    private static long attachmentCounter = 1;
+    private static long reportCounter = 1;
+    
+    // private constructor because methods of this class are used as static methods
+    private IdGenerator() {
+    }
+
+    // get next id for each type
+    public static synchronized String nextUserId() {
+        return "USER-" + userCounter++;
+    }
+    public static synchronized String nextMessageId() {
+        return "Message-" + messageCounter++;
+    }
+    public static synchronized String nextGroupId() {
+        return "Group-" + groupCounter++;
+    }
+    public static synchronized String nextConversationId() {
+        return "Conversation-" + conversationCounter++;
+    }
+    public static synchronized String nextAttachmentId() {
+        return "Attachment-" + attachmentCounter++;
+    }
+    public static synchronized String nextReportId() {
+        return "Report-" + reportCounter++;
+    }
 }
